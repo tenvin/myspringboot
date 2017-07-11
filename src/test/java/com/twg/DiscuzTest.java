07a33ad30516712539d8b589d0f37b21aed3e027
tenvin <tenvin@gmail.com>
2017-07-11 17:07:40 +0800

    update

---
+++ src/test/java/com/twg/DiscuzTest.java
@@ -0,0 +1,20 @@
+package com.twg;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+/**
+ * Created by twg on 2017/7/10.
+ */
+public class DiscuzTest {
+    public static void main(String[] args) {
+        String loginUrl;
+        String formhashUrl;
+        String signUrl;
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        //1.login
+    }
+
+}
