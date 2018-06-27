fd670d0b9d849cd5d852a2d32214b91c4b39c26b
tenvin <tenvin@gmail.com>
2018-06-27 15:45:46 +0800

    update

--- src/main/java/com/twg/MyApplication.java
+++ src/main/java/com/twg/MyApplication.java
@@ -2,13 +2,12 @@ package com.twg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-@EnableCaching
+
 @SpringBootApplication
-public class MyspringbootApplication {
+public class MyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyspringbootApplication.class, args);
+		SpringApplication.run(MyApplication.class, args);
 	}
 }
--- src/test/java/com/twg/MyApplicationTests.java
+++ src/test/java/com/twg/MyApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MyspringbootApplicationTests {
+public class MyApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
